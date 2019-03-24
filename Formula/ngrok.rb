436f923cfaae557b161b48cd2ee9f3ba9a0cb82d
Steven Berg <steven@stevenberg.net>
2019-03-23 20:39:36 -0400

    Update ngrok

--- Formula/ngrok.rb
+++ Formula/ngrok.rb
@@ -1,9 +1,9 @@
 class Ngrok < Formula
   desc "Introspected tunnels to localhost"
   homepage "https://ngrok.com"
-  version "2.3.18"
+  version "2.3.23"
   url "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-darwin-amd64.zip"
-  sha256 "9862ad502f1ca2eab34fc7dbef0b201dd823697f07f519ab5ef0f6be23943f5b"
+  sha256 "24e6cdcced7edb102ab7a1c7ad5e0b179adbf1f26de13560eb5150bf324003dd"
 
   def install
     prefix.install Dir["*"]
